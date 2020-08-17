47548875717746fa9a669a4e532e475a5575e3bd
mitchellreiss <mitchellreiss@gmail.com>
2020-08-17 08:16:56 -0700

    Add css conditionals to dropzone

--- frontend/src/components/dropzone.jsx
+++ frontend/src/components/dropzone.jsx
@@ -1,23 +1,34 @@
-import React, {useCallback} from 'react'
-import {useDropzone} from 'react-dropzone'
-import { Button } from "react-bootstrap";
- 
-const Dropzone = () => {
-  const onDrop = useCallback(acceptedFiles => {
-		debugger
-  }, [])
-  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
-	
-		let color = isDragActive ? "success" : "warning"
- 
+import React from "react";
+import Dropzone from "react-dropzone";
+import { FileConsumer } from "../file_context";
+
+export default function DropzoneComponent() {
   return (
-    <Button variant={color} {...getRootProps()}>
-      <input {...getInputProps()} />
-			Upload Workout Data Here
-     
-    </Button>
-  )
+    <FileConsumer>
+      {({ updateFile }) => (
+        <Dropzone
+          onDrop={(newFile) => {
+            updateFile(newFile);
+          }}
+        >
+          {({ getRootProps, getInputProps, isDragActive }) => (
+            <div
+              style={{
+                border: `5px dashed ${isDragActive ? "green" : "white"}`,
+                color: `${isDragActive ? "green" : "white"}`,
+              }}
+              {...getRootProps()}
+              id="upload-button"
+            >
+              <input {...getInputProps()} />
+              <h1 className="upload-header">
+                Upload GPX data by clicking to browse your files or drag and
+                drop here!
+              </h1>
+            </div>
+          )}
+        </Dropzone>
+      )}
+    </FileConsumer>
+  );
 }
-
-export default Dropzone
-
