12fac4da7546e498fe70adf1b9c29bfb6073ced7
mitchellreiss <mitchellreiss@gmail.com>
2020-08-17 08:45:11 -0700

    Navbar now updates when file context changes

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -2,19 +2,32 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Button from "react-bootstrap/button";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
+import { FileConsumer } from "../file_context";
 
 export default function NavBar() {
-  
+  const noDataButtons = (
+    <ButtonGroup>
+      <Button variant="secondary">Download Sample Data</Button>
+      <Button variant="secondary">Sample Dashboard</Button>
+    </ButtonGroup>
+  );
+
+  const resetDataButton = (updateFile) => (
+    <Button variant="secondary" onClick={() => updateFile(null)}>
+      Upload New File
+    </Button>
+  );
 
   return (
-    <Navbar bg="primary" variant="dark">
-      <span class="navbar-span">
-        <Navbar.Brand>Map Visualizer</Navbar.Brand>
-        <ButtonGroup>
-          <Button variant="secondary">Download Sample Data</Button>
-          <Button variant="secondary">Sample Dashboard</Button>
-        </ButtonGroup>
-      </span>
-    </Navbar>
+    <FileConsumer>
+      {({ file, updateFile }) => (
+        <Navbar bg="primary" variant="dark">
+          <span class="navbar-span">
+            <Navbar.Brand>Map Visualizer</Navbar.Brand>
+            {file ? resetDataButton(updateFile) : noDataButtons}
+          </span>
+        </Navbar>
+      )}
+    </FileConsumer>
   );
 }
