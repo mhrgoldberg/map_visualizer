0c8bb8937b6c80e97cf3a75b3cd564c03684865d
mitchellreiss <mitchellreiss@gmail.com>
2020-08-22 11:57:14 -0700

    Created custom useMap hook

---
+++ frontend/src/hooks/useMap_hook.js
@@ -0,0 +1,43 @@
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  useContext,
+} from "react";
+
+import { FileContext } from "../file_context";
+
+
+export default function useMap() {
+	const ref = useRef();
+  const [map, setMap] = useState();
+  const { state } = useContext(FileContext);
+  const [options] = useState({
+    center: {
+      lat: state.file.trackPoints.latLngs[0].lat,
+      lng: state.file.trackPoints.latLngs[0].lng,
+    },
+    zoom: 15,
+  });
+
+
+
+
+  useEffect(() => {
+    const onLoad = () =>
+      setMap(new window.google.maps.Map(ref.current, { ...options }));
+    if (!window.google) {
+      const script = document.createElement(`script`);
+      script.src =
+        `https://maps.googleapis.com/maps/api/js?key=` +
+        process.env.REACT_APP_GOOGLE_API_KEY;
+      document.head.append(script);
+      script.addEventListener(`load`, onLoad);
+      return () => script.removeEventListener(`load`, onLoad);
+    } else onLoad();
+  }, [options]);
+
+ 
+	
+	return [ref, map]
+}
