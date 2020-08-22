cc3a12728085c30dc7d1222b3dbc7ad4d01faa47
mitchellreiss <mitchellreiss@gmail.com>
2020-08-22 11:57:28 -0700

    Create cutom usePolyline component

---
+++ frontend/src/hooks/usePolyline_hook.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useCallback, useContext } from "react";
+import { FileContext } from "../file_context";
+
+export default function usePolyline(map) {
+	const { state } = useContext(FileContext);
+  const addPolyline = useCallback(() => {
+    const latLngPath = state.file.trackPoints.latLngs;
+    const workoutPath = new window.google.maps.Polyline({
+      path: latLngPath,
+      geodesic: true,
+      strokeColor: "#FF0000",
+      strokeOpacity: 1.5,
+      strokeWeight: 2,
+    });
+    const bounds = new window.google.maps.LatLngBounds();
+    latLngPath.forEach((trackPoint) => bounds.extend(trackPoint));
+    map.setCenter(bounds.getCenter());
+    map.fitBounds(bounds);
+    workoutPath.setMap(map);
+  }, [state.file.trackPoints.latLngs, map]);
+
+  useEffect(() => {
+    if (map) addPolyline();
+  }, [map, addPolyline]);
+}
